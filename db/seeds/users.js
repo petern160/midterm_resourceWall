f0cb0b4fd486e44e18276f7565f5a96a4458ad30
petern160 <peternguyen160@gmail.com>
2019-04-25 02:28:12 +0000

    added working seed for users.js also created other empty seed files

--- db/seeds/users.js
+++ db/seeds/users.js
@@ -2,9 +2,11 @@ exports.seed = function(knex, Promise) {
   return knex('users').del()
     .then(function () {
       return Promise.all([
-        knex('users').insert({id: 1, name: 'Alice'}),
-        knex('users').insert({id: 2, name: 'Bob'}),
-        knex('users').insert({id: 3, name: 'Charlie'})
+        knex('users').insert({id: 1, first_name: 'Alice', last_name: 'Red', email: 'ar@test.com', password: 'test'}),
+        knex('users').insert({id: 2, first_name: 'Joe', last_name: 'Blue', email: 'jb@test.com', password: 'test2'}),
+        knex('users').insert({id: 3, first_name: 'Dave', last_name: 'Green', email: 'dg@test.com', password: 'test3'})
       ]);
     });
 };
+
+
